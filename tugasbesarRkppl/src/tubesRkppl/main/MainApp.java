2b11fc4fd4ef7564ec9dbd6972dbd7b9a81e0686
Dianda Kacinta <dianda.kacinta@gmail.com>
2014-06-09 18:41:01 +0700

    yg masih kurang : class E (list & set-get), class C (pengaksesan class A-B-Math), sama class main (pengaksesan class C-D-E)

---
+++ tugasbesarRkppl/src/tubesRkppl/main/MainApp.java
@@ -0,0 +1,19 @@
+package tubesRkppl.main;
+
+import tubesRkppl.kelas.C;
+import tubesRkppl.kelas.E;
+
+/**
+ * @author 
+ * Dianda Kacinta
+ * 113040249
+ */
+public class MainApp {
+
+	//Membuat method main yang akan menjalankan keseluruhan program
+	public static void main(String[] arr) {
+		C objekC = new C();
+		
+		E objekE = new E();
+	}
+}
