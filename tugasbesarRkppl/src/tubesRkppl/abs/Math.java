09ef2dfca23b6c9513750ed684b5b88722e5be92
PuputNurovy <nurovypuput@ymail.com>
2014-06-10 07:21:11 +0700

    testing

--- tugasbesarRkppl/src/tubesRkppl/abs/Math.java
+++ tugasbesarRkppl/src/tubesRkppl/abs/Math.java
@@ -1,11 +1,18 @@
 package tubesRkppl.abs;
 
+/**
+ * @author 
+ * Dianda Kacinta
+ * 113040249
+ */
 public abstract class Math {
 
+	//Membuat function mod yang akan mengembalikan nilai mod dari bilangan parameter n1 & n2
 	public int mod(int n1, int n2){
-		return 0;
+		return n1%n2;
 	}
 	
+	//Membuat function sqrt
 	public double sqrt(double n){
 		return 0;
 	}
---
+++ tugasbesarRkppl/src/tubesRkppl/kelas/C.java
@@ -0,0 +1,44 @@
+package tubesRkppl.kelas;
+
+/**
+ * @author 
+ * Dianda Kacinta
+ * 113040249
+ */
+public class C {
+
+	//Membuat variable class
+	double n1;
+	double n2;
+	
+	//Membuat konstruktor class
+	public C() {
+		// TODO Auto-generated constructor stub
+		System.out.println("Ctor di kelas C");
+	}
+	
+	//Membuat function add yang akan mengembalikan hasil penambahan n1 & n2
+	public int add(){
+		return (int)(n1+n2);
+	}
+	
+	//Membuat function sub yang akan mengembalikan hasil pengurangan n1 & n2
+	public int sub(){
+		return (int)(n1-n2);
+	}
+	
+	//Membuat function mul yang akan mengembalikan hasil perkalian n1 & n2
+	public double mul(){
+		return n1*n2;
+	}
+	
+	//Membuat function div yang akan mengembalikan hasil pembagian n1 & n2
+	public double div(){
+		return n1/n2;
+	}
+	
+	//Membuat function toString
+	public String toString(){
+		return "toString di kelas C";
+	}
+}
