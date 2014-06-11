c6d3ae8029f3bee6d72f273504b831e531c29389
PuputNurovy <nurovypuput@ymail.com>
2014-06-11 20:12:25 +0700

    @author puput nurovy kelas B

--- tugasbesarRkppl/src/tubesRkppl/kelas/B.java
+++ tugasbesarRkppl/src/tubesRkppl/kelas/B.java
@@ -10,12 +10,12 @@ public class B implements MulAndDivInterface{
 	
 	public double mul(double n1, double n2){
 		
-		return 0;
+		return n1*n2;
 	}
 	
 	public double div(double n1, double n2){
 
-		return 0;
+		return n1/n2;
 	}
 
 }
