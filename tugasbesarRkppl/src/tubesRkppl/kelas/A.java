d1b8c61d574c6956d2168704dff6387af5881245
PuputNurovy <nurovypuput@ymail.com>
2014-06-11 20:24:59 +0700

    @author puput nurovy : kelas A dan B

--- tugasbesarRkppl/src/tubesRkppl/kelas/A.java
+++ tugasbesarRkppl/src/tubesRkppl/kelas/A.java
@@ -1,20 +1,28 @@
 package tubesRkppl.kelas;
 
+/**
+ * @author 
+ * Puput Nurovy
+ * 113040155
+ */ 
+
 import tubesRkppl.itface.AddAndSubInterface;
 
 public class A implements AddAndSubInterface{
 
 	public A() {
-		// TODO Auto-generated constructor stub
+		System.out.println("Ctor di kelas B");
 	}
 	
 	public int add(int n1, int n2){
-	
-		return 0;
+
+		// fungsi untuk mengembalikan pertambahan 2 nilai
+		return n1+n2;
 	}
 	
 	public int sub(int n1, int n2){
-		
-		return 0;
+
+		// fungsi untuk mengembalikan pengurangan 2 nilai
+		return n1-n2;
 	}
 }
--- tugasbesarRkppl/src/tubesRkppl/kelas/B.java
+++ tugasbesarRkppl/src/tubesRkppl/kelas/B.java
@@ -1,18 +1,28 @@
-package tubesRkppl.kelas;
+package
+
+/**
+ * @author 
+ * Puput Nurovy
+ * 113040155
+ */ 
+
+tubesRkppl.kelas;
 
 import tubesRkppl.itface.MulAndDivInterface;
 
 public class B implements MulAndDivInterface{
 
 	public B() {
-		// TODO Auto-generated constructor stub
+		System.out.println("Ctor di kelas B");
 	}
 	
+	// fungsi untuk mengembalikan perkalian 2 nilai
 	public double mul(double n1, double n2){
 		
 		return n1*n2;
 	}
-	
+
+	// fungsi untuk mengembalikan pembagian 2 nilai
 	public double div(double n1, double n2){
 
 		return n1/n2;
